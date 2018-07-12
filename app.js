032522b1b3d6797967c6ced771698f218d8d1c72
rkwizdev <rahulkumarkorada@gmail.com>
2018-07-13 05:01:44 +0530

    added session store

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
@@ -34,7 +35,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret:'mykey',resave : false , saveUninitialized : false}));
+app.use(session({
+  secret:'mykey',
+  resave : false ,
+  saveUninitialized : false,
+  store : new MongoStore({ mongooseConnection : mongoose.connection }),
+  cookie : {10 * 1000}
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -42,6 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
   res.locals.login = req.isAuthenticated() ;
+  res.locals.session = req.session;
   next();
 });
 
